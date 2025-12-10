523efacd7fb39ba40496814380b03667e3b0475e
阿杰 <96617883+ZZHENJIE@users.noreply.github.com>
2025-12-10 21:33:58 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ use std::sync::Arc;
 async fn main() -> anyhow::Result<()> {
     // init settings and server config
     let settings = dtbox_lib::Settings::new("./settings.toml")?;
-    let server_host_port = format!("127.0.0.1:{}", settings.server.port);
+    let server_host_port = format!("0.0.0.0:{}", settings.server.port);
     // create state and router
     let state = Arc::new(dtbox_lib::AppState::new(settings).await?);
     let app = dtbox_lib::Router::new(state.settings()).with_state(Arc::clone(&state));
     // verify listener and start server
     let listener = tokio::net::TcpListener::bind(&server_host_port).await?;
     println!(
-        "Server created successfully.\nListener http://{}/static 🎉",
-        server_host_port
+        "Server created successfully.\nListener http://127.0.0.1:{}/static 🎉",
+        state.settings().server.port
     );
     axum::serve(listener, app).await.unwrap_or_else(|err| {
         eprintln!("Axum server error: {}", err);
