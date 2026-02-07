72860f9d724af3ea4670ed948a8678ad577d267c
ZHONGZHENJIE <508916879@qq.com>
2026-02-08 07:13:55 +0800

    update

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,6 +1,5 @@
-use anyhow::Context;
 use chrono::Local;
-use std::fs::File;
+use std::{fs::File, path::Path};
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{Layer, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -12,7 +11,11 @@ pub fn log_system_init() -> anyhow::Result<LogGuard> {
     let log_dir = "logs";
     let log_file = format!("{}/{}.log", log_dir, Local::now().format("%Y-%m-%d"));
 
-    let file = File::create(log_file).with_context(|| "Failed to create log file")?;
+    if let Some(parent) = Path::new(&log_file).parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+
+    let file = File::create(&log_file)?;
     let (non_blocking, guard) = tracing_appender::non_blocking(file);
 
     let file_layer = fmt::layer().with_writer(non_blocking).boxed();
